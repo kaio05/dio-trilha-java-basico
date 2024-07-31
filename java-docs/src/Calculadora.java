822bd32f6be07b0b6b977393db34dbf0ed3397c8
Kaio <kaiofernando05cont@gmail.com>
2024-07-31 14:34:38 -0300

    aulas java docs e java terminal

---
+++ java-docs/src/Calculadora.java
@@ -0,0 +1,52 @@
+/**
+* <h1>Calculadora</h1>
+* A Calculadora realiza operações matemáticas entre números inteiros
+* <p>
+* <b>Note:</b> Leia atentamente a documentação desta classes
+* para desfrutar dos recursos oferecidos pelo autor
+*
+* @author  Kaio Fernando
+* @version 1.0
+* @since   30/07/2024
+*/
+public class Calculadora {
+    /**
+   * Este método é utilizado para somar dois números inteiros
+   * @param numeroUm este é o primeiro parâmetro do método
+   * @param numeroDois este é o segundo parâmetro do método
+   * @return int o resultado deste método é a soma dos dois números.
+   */
+    public int somar(int numeroUm, int numeroDois) {
+        return  numeroUm + numeroDois;
+    }
+    
+    /**
+   * Este método é utilizado para somar subtrair números inteiros
+   * @param numeroUm este é o primeiro parâmetro do método
+   * @param numeroDois este é o segundo parâmetro do método
+   * @return int o resultado deste método é a diferença dos dois números.
+   */
+    public int subtrair(int numeroUm, int numeroDois) {
+        return numeroUm - numeroDois;
+    }
+
+    /**
+   * Este método é utilizado para multiplicar dois números inteiros
+   * @param numeroUm este é o primeiro parâmetro do método
+   * @param numeroDois este é o segundo parâmetro do método
+   * @return int o resultado deste método é o produto dos dois números.
+   */
+    public int multiplicar(int numeroUm, int numeroDois) {
+        return numeroUm * numeroDois;
+    }
+
+    /**
+   * Este método é utilizado para dividir dois números inteiros
+   * @param numeroUm este é o primeiro parâmetro do método
+   * @param numeroDois este é o segundo parâmetro do método
+   * @return int o resultado deste método é o quociente dos dois números.
+   */
+    public int dividir(int numeroUm, int numeroDois) {
+        return numeroUm / numeroDois;
+    }
+}
